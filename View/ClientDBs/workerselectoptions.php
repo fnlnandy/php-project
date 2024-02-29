c94d60e4ab49ea2a46e24776b59558960bd1596a
fnlnandy <andyfaniloniaina01@gmail.com>
2024-02-29 07:10:21 +0300

    Updated worker clientDB to load only valid workers

--- View/ClientDBs/workerselectoptions.php
+++ View/ClientDBs/workerselectoptions.php
@@ -1,7 +1,7 @@
 <?php
     class WorkerSelectOptions {
         public static function PopulateSelectOptionIds() {
-            $query = SQLQuery::ExecQuery("SELECT NumEmp FROM EMPLOYE ORDER BY LENGTH(NumEmp) ASC, NumEmp ASC;");
+            $query = SQLQuery::ExecQuery("SELECT NumEmp FROM EMPLOYE WHERE Lieu IN (SELECT IDLieu FROM LIEU) ORDER BY LENGTH(NumEmp) ASC, NumEmp ASC;");
 
             if (!$query || is_null($query))
                 return;
@@ -15,7 +15,7 @@
             }
         }
         public static function PopulateSelectOptionNames() {
-            $query = SQLQuery::ExecQuery("SELECT Nom, Prenom FROM EMPLOYE ORDER BY LENGTH(NumEmp) ASC, NumEmp ASC;");
+            $query = SQLQuery::ExecQuery("SELECT Nom, Prenom FROM EMPLOYE WHERE Lieu IN (SELECT IDLieu FROM LIEU) ORDER BY LENGTH(NumEmp) ASC, NumEmp ASC;");
 
             if (!$query || is_null($query))
                 return;
